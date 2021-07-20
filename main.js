62cd038a195e2e1e693fac7913638c65613b14db
KinGOfGamerZ439 <77156036+KinGOfGamerZ439@users.noreply.github.com>
2021-07-20 15:58:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function setup() 
+{
+     createCanvas(649, 480);
+     canvas.position(110, 250);
+     video=createCapture(VIDEO);
+     video.hide();
+     color="";
+}
+
+function draw() 
+{
+    image(img, 10, 10, 500, 520);
+    tint(color);
+
+}
+function take_snapshot()
+{
+    save('filter.png');
+    k7v23v3oku.scalacube.pro;
+
+}
